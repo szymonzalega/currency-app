dbe02c5a8752610313705d523721b0971d2b003d
Szymon.Zalega <szymonzalega95@gmail.com>
2020-04-11 22:05:19 +0200

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="header">
+    <div>Header</div>
+    <p>{{ sample }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    sample: String
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+#header {
+  height: 60px;
+  background-color: #1d2432;
+  padding: 3px;
+  border: 2px dashed #fff;
+  color: #fff;
+}
+</style>
