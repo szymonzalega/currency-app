ebef264f66d0a13abcf7b1958711538174e38964
Mateusz Szopa <github@mateusz-szopa.pl>
2020-04-25 14:59:32 +0200

    Dodanie modala do wybierania nowej waluty do wyświetlenia

--- src/components/NewCurrency.vue
+++ src/components/NewCurrency.vue
@@ -1,7 +1,41 @@
 <template>
-  <div id="newCurrency">
-      {{ data }}
-      //{{ currencies }}
+  <div>
+    <b-button v-b-modal.modal-prevent-closing> Dodaj walutę </b-button>
+
+    <b-modal
+      id="modal-prevent-closing"
+      ref="modal"
+      title="Dodaj nową walutę"
+      @show="resetModal"
+      @hidden="resetModal"
+      @ok="handleOk"
+    >
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+        <b-form-group
+          label="Nazwa waluty"
+          label-for="currency-select"
+        >
+        <b-form-select 
+          v-model="selected" 
+          :options="options" 
+          class="mb-3"> </b-form-select>
+        </b-form-group>       
+      
+        <b-form-group
+          label="Liczba dni"
+          label-for="currency-perioid"
+        >
+        <b-form-input 
+          v-model="timePerioid" 
+          min="1" 
+          max="255" 
+          id="dayAmount" 
+          size="sm"
+          type="number"></b-form-input>
+        </b-form-group>
+        
+      </form>
+    </b-modal>
   </div>
 </template>
 
@@ -10,7 +44,10 @@ export default {
   name: 'NewCurrency',
   data() {
     return {
-      currencies: []
+      currencies: {},
+      options: [],
+      selected: '',
+      timePerioid: null
     };
   },
   created() {
@@ -18,8 +55,36 @@ export default {
   },
   methods: {
     getCurrenciesFromStore() {
-      this.$store.dispatch("currency/fetchCurrencies");
       this.currencies = this.$store.getters["currency/getCurrencies"];
+      this.getCurrenciesList();
+    }, 
+    getCurrenciesList() {
+      console.log('abc');
+      console.log(this.currencies.length)
+      for(var i = 0; i < this.currencies.length; i++){
+        var option = []
+        for(var key in this.currencies[i]){
+          if(key == "code"){
+            option["value"] = this.currencies[i][key]
+          }else if(key == "currency"){
+            option["text"] = this.currencies[i][key]
+          }
+        }
+      this. options.push(Object.assign({},option))
+      }
+    },
+    resetModal() {
+        this.selected = ''
+        this.timePerioid = null
+    },
+    handleOk(bvModalEvt) {
+        bvModalEvt.preventDefault()
+        this.handleSubmit()
+    },
+    handleSubmit() {
+        this.$nextTick(() => {
+          this.$bvModal.hide('modal-prevent-closing')
+        })
     }
   },
   props: {
@@ -27,14 +92,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-    #newCurrency {
-        height: 50px;
-        width: 50vw;
-        margin: 1em;
-        background-color: #33394c;
-    }
-
-</style>
