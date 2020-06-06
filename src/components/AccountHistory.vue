19444d2f40379c084708ddfdab8ef48a2fb23e81
Mateusz Szopa <github@mateusz-szopa.pl>
2020-06-06 22:05:48 +0200

    Wyświetlenie tabelki z audytem

--- src/components/AccountHistory.vue
+++ src/components/AccountHistory.vue
@@ -1,29 +1,150 @@
 <template>
-  <div class="balance-dashboard">
-    <view-name name="Historia operacji" />
-  </div>
+  <b-container>
+    <b-row style="padding:30px">
+    <b-col lg="6" class="my-1">
+        <b-form-group
+          label="Filter"
+          label-cols-sm="3"
+          label-align-sm="right"
+          label-size="sm"
+          label-for="filterInput"
+          class="mb-0"
+        >
+          <b-input-group size="sm">
+            <b-form-input
+              v-model="filter"
+              type="search"
+              id="filterInput"
+              placeholder="Type to Search"
+            ></b-form-input>
+            <b-input-group-append>
+              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+
+      <b-col lg="6" class="my-1">
+            <b-button> Wydrukuj do pdf </b-button>
+      </b-col>
+    </b-row>
+
+    <!-- Main table element -->
+    <b-table dark
+      show-empty
+      small
+      stacked="md"
+      :items="items"
+      :fields="fields"
+      :current-page="currentPage"
+      :per-page="perPage"
+      :filter="filter"
+      :filterIncludedFields="filterOn"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      :sort-direction="sortDirection"
+      @filtered="onFiltered"
+    >   
+
+      <template v-slot:row-details="row">
+        <b-card>
+          <ul>
+            <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
+          </ul>
+        </b-card>
+      </template>
+    </b-table>
+    <b-row>
+              <b-col sm="5" md="6" class="my-1">
+        <b-form-group
+          label="Per page"
+          label-cols-sm="6"
+          label-cols-md="4"
+          label-cols-lg="3"
+          label-align-sm="right"
+          label-size="sm"
+          label-for="perPageSelect"
+          class="mb-0"
+        >
+          <b-form-select
+            v-model="perPage"
+            id="perPageSelect"
+            size="sm"
+            :options="pageOptions"
+          ></b-form-select>
+        </b-form-group>
+      </b-col>
+
+      <b-col sm="7" md="6" class="my-1">
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="totalRows"
+          :per-page="perPage"
+          align="fill"
+          size="sm"
+          class="my-0"
+        ></b-pagination>
+      </b-col>  
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-import ViewName from "./ViewName.vue";
-export default {
-  name: "AccountHistory",
-  data: function() {
-    return {
-    };
-  },
-  props: {},
-  components: {
-    ViewName
-  },
-  computed: {},
-  created() {
-  },
-  methods: {
- }
-};
+  export default {
+    data() {
+      return {
+        items: [],
+        fields: [
+          { key: 'time', label: 'Data', sortable: true, sortDirection: 'desc' },
+          { key: 'event', label: 'Event', sortable: true, class: 'text-center' },
+        ],
+        totalRows: 1,
+        currentPage: 1,
+        perPage: 20,
+        pageOptions: [5, 10, 15, 20, 25, 30, 35],
+        sortBy: '',
+        sortDesc: false,
+        sortDirection: 'asc',
+        filter: null,
+        filterOn: [],
+      }
+    },
+    created() {
+        console.log('created')
+        let userId = this.$store.getters["user/user"].uid;
+        console.log(userId)
+         this.$store
+        .dispatch("audit/getAuditRecords", userId)
+        .then(() => {
+          this.items = this.$store.getters[
+            "audit/getAudit"
+          ];
+            this.totalRows = this.items.length
+        });
+    },
+    computed: {
+      sortOptions() {
+        return this.fields
+          .filter(f => f.sortable)
+          .map(f => {
+            return { text: f.label, value: f.key }
+          })
+      }
+    },
+    methods: {
+      info(item, index, button) {
+        this.infoModal.title = `Row index: ${index}`
+        this.infoModal.content = JSON.stringify(item, null, 2)
+        this.$root.$emit('bv::show::modal', this.infoModal.id, button)
+      },
+      resetInfoModal() {
+        this.infoModal.title = ''
+        this.infoModal.content = ''
+      },
+      onFiltered(filteredItems) {
+        this.totalRows = filteredItems.length
+        this.currentPage = 1
+      },
+    }
+  }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-</style>
